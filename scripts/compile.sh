95a4c773a6e855290504932fb43adeb39f029239
pigtamer <1617752166@qq.com>
2019-03-12 15:38:35 +0800

    adapt ./scripts/compile.sh and ./lib... for python 3

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo 'Compiling NMS module...'
-(cd lib/nms; python setup_linux.py build_ext --inplace)
+(cd lib/nms; python3 setup_linux.py build_ext --inplace)
 echo 'Compiling bbox module...'
-(cd lib/bbox; python setup_linux.py build_ext --inplace)
+(cd lib/bbox; python3 setup_linux.py build_ext --inplace)
 echo 'Compiling chips module...'
-(cd lib/chips; python setup.py)
+(cd lib/chips; python3 setup.py)
 echo 'Compiling coco api...'
-(cd lib/dataset/pycocotools; python setup_linux.py build_ext --inplace)
+(cd lib/dataset/pycocotools; python3 setup_linux.py build_ext --inplace)
 echo 'All Done!'
